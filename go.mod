67a26015c18681eb129c1a5df8cec85d1d2f1b44
QJoly <github@thoughtless.eu>
2024-01-03 15:56:56 +0100

    Refactor CLI and add config file generation

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
-	github.com/qjoly/burntcoffee/pkgs/firec v0.0.0-20240103142514-087d476ca441 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- main.go
+++ main.go
@@ -1,22 +1,100 @@
 package main
 
 import (
-	cmd "github.com/qjoly/burntcoffee/cmd"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Usage: ./run_job IP:PORT")
-	// 	os.Exit(1)
-	// }
-	cmd.Execute()
 
-	// job, err := findUnstartedVMs(os.Args[1:])
-	// if err != nil {
-	// 	fmt.Println("Error finding unstarted VMs:", err)
-	// 	os.Exit(1)
-	// }
+	Execute()
 
-	// fmt.Println("Job started on", job)
+}
 
+var (
+	// Used for flags.
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "coffeeburn",
+		Short: "A way to manage firecracker VMs",
+		Long:  `burntCoffee is a cli application to manage a remote firecracker socket exposed by a socat proxy.`,
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
+	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+	viper.SetDefault("license", "apache")
+
+	// show functions in package configfile
+
+	initConfigCmd := &cobra.Command{
+		Use:   "init-config",
+		Short: "Initialize the configuration",
+		Run: func(cmd *cobra.Command, args []string) {
+			initConfig()
+		},
+	}
+
+	genConfigCmd := &cobra.Command{
+		Use:   "gen-config",
+		Short: "Generate the configuration file",
+		Run: func(cmd *cobra.Command, args []string) {
+			config
+		},
+	}
+
+	fmt.Println(getActualStatus())
+
+	job := &cobra.Command{
+		Use:   "start-job",
+		Short: "Start a job on a VM",
+		Run: func(cmd *cobra.Command, args []string) {
+			// from github.com/qjoly/burntcoffee/pkgs/firec/firec.go
+
+		},
+	}
+
+	rootCmd.AddCommand(initConfigCmd)
+	rootCmd.AddCommand(job)
+	rootCmd.AddCommand(genConfigCmd)
+
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
