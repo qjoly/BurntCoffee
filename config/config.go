b3f22a1519cf9e2f1373d96df6870b0c1acf447d
QJoly <github@thoughtless.eu>
2024-01-05 10:42:52 +0100

    Fix error message in GetConfig function

--- config/config.go
+++ config/config.go
@@ -90,7 +90,7 @@ func GetConfig(cfgFile string) Config {
 	fmt.Println("Using config file:", cfgFile)
 	yamlFile, err := os.ReadFile(cfgFile)
 	if err != nil {
-		fmt.Printf("Config file not found: %s \n You can generate a config-file using ./burntcoffee gen-config", err)
+		fmt.Printf("Config file not found: %s \n You can generate a config-file using ./burntcoffee gen-config \n", err)
 		os.Exit(1)
 	}
 
